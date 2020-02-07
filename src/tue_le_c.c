6a6b60a6b6242a98074b0b18808f10fd14832f8a
Anne Géraud <kally@localhost.localdomain>
2020-02-07 02:14:32 +0100

    Mapping dans une structure

--- src/tue_le_c.c
+++ src/tue_le_c.c
@@ -1,21 +1,74 @@
 #include "tue_le_c.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
-	FILE *file = NULL;
-	char *line = NULL;
+	FILE* file = NULL;
+	char* line = NULL;
 	size_t line_size = 0;
+	product_t product;
 
 	file = fopen("data/tue_le_c.csv", "r");
 
 	if (file != NULL) {
 		while (getline(&line, &line_size, file) > 0) {
-			printf("%s", line);
+			if(*line != '#') {
+				parse_line(line, &product);
+				print_product(product);
+			}
 		}
+		free(line);
 		fclose(file);
 		return 0;
 	} else {
 		printf(OPEN_ERROR, "data/tue_le_c.csv");
 		return 1;
 	}
+}
+
+int parse_line(char* line, product_t* product) 
+{
+	char* token;
+
+	token = strtok(line, ",");
+	strcpy(product->marque, token);
+
+	token = strtok(NULL, ",");
+	strcpy(product->modele, token);
+
+	token = strtok(NULL, ",");
+	product->ref_fournisseur = atoi(token);
+
+	token = strtok(NULL, ",");
+	product->ref_sap = atoi(token);
+
+	token = strtok(NULL, ",");
+	strcpy(product->designation, token);
+
+	token = strtok(NULL, ",");
+	product->produits_par_carton = atoi(token);
+
+	token = strtok(NULL, ",");
+	product->prix_achat = atof(token);
+
+	token = strtok(NULL, ",");
+	product->prix_vente = atof(token);
+
+	token = strtok(NULL, ",\n");
+	product->code_barre = atol(token);
+
+	return(0);
+}
+
+void print_product(product_t product)
+{
+	printf(PRODUCT_FORMAT,
+			product.ref_fournisseur,
+			product.ref_sap,
+			product.designation,
+			product.produits_par_carton,
+			product.code_barre,
+			product.prix_achat,
+			product.prix_vente,
+			product.modele,
+			product.marque); 
 }
--- src/tue_le_c.h
+++ src/tue_le_c.h
@@ -1,17 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define OPEN_ERROR "Impossible d'ouvrir le fichier %s\n"
 
+#define PRODUCT_FORMAT "product:\n\tref_fournisseur: %d\n\tref_sap: %d\n\tdesignation: %s\n\tproduits_par_carton: %d\n\tcode_barre: %ld\n\tprix_achat: %f\n\tprix_vente: %f\n\tmodele: %s\n\tmarque: %s\n\n"
+
 typedef struct {
-	int ref_fournisseur; 
-	char designation[100];
-	int produits_par_carton;
-	int code_barre;
-	double prix_achat;
-	double prix_vente;
-	char modele[100];
-	char marque[100];
-} product;
+	int	ref_fournisseur; 
+	int	ref_sap;
+	char	designation[100];
+	int	produits_par_carton;
+	long	code_barre;
+	double	prix_achat;
+	double	prix_vente;
+	char	modele[100];
+	char	marque[100];
+} product_t;
 
+int parse_line(char*, product_t*);
 
+void print_product(product_t);
